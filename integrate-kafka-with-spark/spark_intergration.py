1c98eea7520f82fec58d3fe8d114d23acf04a3d1
ahmed-hassan97 <ahmed1997abohassan@gmail.com>
2023-02-01 11:17:26 +0200

    add spark integration

--- integrate-kafka-with-spark/spark_intergration.py
+++ integrate-kafka-with-spark/spark_intergration.py
@@ -8,6 +8,20 @@ import time
 scala_version = '2.12'
 spark_version = '3.1.2'
 
+def postgres_config():
+    dbhost = "localhost",
+    dbport = 5432
+    dbname = "test"
+    dbuser = "postgres"
+    dbpass = "postgres"
+    url = "jdbc:postgresql://"+str(dbhost)+":"+str(dbport)+"/"+str(dbname)
+    properties = {
+        "driver": "org.postgresql.Driver",
+        "user": dbuser,
+        "password": dbpass
+    }
+    return url , properties
+    
 # TODO: Ensure match above values match the correct versions
 packages = [
     f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
@@ -40,10 +54,19 @@ json_df = kafka_df\
             .select(psf.from_json(psf.col('value'), jsonSchema).alias("kafka-spark-integration"))\
             .select("kafka-spark-integration.*")
 
-query = json_df\
-            .writeStream \
-            .outputMode("append") \
-            .format("console") \
-            .start()
+# query = json_df\
+#             .writeStream \
+#             .outputMode("append") \
+#             .format("console") \
+#             .start()
 
-query.awaitTermination()
+
+
+json_df.writeStream.outputMode("append")\
+            .format("json")\
+            .option("path", "/output_files/")\
+            .option("header", True)\
+            .option("checkpointLocation", "/output_files/checkpoint/")\
+            .start()\
+            .awaitTermination()
+
